3e629adab294b08195a816b44206cfdd44c21d25
db97531 <db97531@gmail.com>
2017-03-18 23:47:55 +0900

    [add] add .Xdefaults for urxvt

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/dein.toml ~/.vim/dein.toml
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitignore ~/.gitignore
+ln -sf ~/dotfiles/.Xdefaults ~/.Xdefaults
 
 git config --global core.excludesfile ~/.gitignore
