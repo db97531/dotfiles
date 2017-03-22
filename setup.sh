9d163d9e559b4eda475211f14c739afa0999b841
db97531 <db97531@gmail.com>
2017-03-22 23:27:39 +0900

    [add] add .xbindkeysrc for xbindkeys.

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ ln -sf ~/dotfiles/dein.toml ~/.vim/dein.toml
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitignore ~/.gitignore
 ln -sf ~/dotfiles/.Xdefaults ~/.Xdefaults
+ln -sf ~/dotfiles/.xbindkeysrc ~/.xbindkeysrc
 ln -sf ~/dotfiles/i3/config ~/.config/i3/config
 
 git config --global core.excludesfile ~/.gitignore
