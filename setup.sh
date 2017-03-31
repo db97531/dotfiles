f100e63c611bbdb2a389fd6c6517897a0974c288
db97531 <db97531@gmail.com>
2017-03-31 17:41:33 +0900

    [add] add .editorconfig and editorconfig plugin in dein.toml.

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitignore ~/.gitignore
 ln -sf ~/dotfiles/.Xdefaults ~/.Xdefaults
 ln -sf ~/dotfiles/.xbindkeysrc ~/.xbindkeysrc
+ln -sf ~/dotfiles/.editorconfig ~/.editorconfig
 ln -sf ~/dotfiles/i3/config ~/.config/i3/config
 
 git config --global core.excludesfile ~/.gitignore
