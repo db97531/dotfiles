8de9751d51cf408bd2c169a6f8e29d20a8732837
db97531 <db97531@gmail.com>
2018-04-19 00:11:44 +0900

    Fix docker.sh

--- docker.sh
+++ docker.sh
@@ -11,3 +11,6 @@ sudo apt-get install -y docker-engine
 # Docker-compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# 現行ユーザをdockerグループに所属させる
+sudo gpasswd -a $USER docker
