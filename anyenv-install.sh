39de343d7afc56207b596264faefd970c485c724
db97531 <db97531@gmail.com>
2018-06-04 02:34:25 +0900

    Fix anyenv-install.sh

--- anyenv-install.sh
+++ anyenv-install.sh
@@ -1,12 +1,19 @@
 #!/bin/bash -e
 
+# phpenv PHPビルド依存プログラムのインストール
+sudo apt install -y libxml2-dev
+sudo apt install -y libcurl-dev
+sudo apt install -y libcurl4-gnutls-dev
+sudo apt install -y libjpeg-dev
+sudo apt install -y libtidy-dev
+sudo apt install -y libxslt-dev
+sudo apt install -y autoconf
+
 pyenv install 3.6.4
 pyenv global 3.6.4
 pyenv rehash
 
 pyenv install 2.7.14
-pyenv global 2.7.14
-pyenv rehash
 
 nodenv install 8.11.2
 nodenv global 8.11.2
@@ -19,15 +26,6 @@ plenv rehash
 rbenv install 2.5.0
 rbenv global 2.5.0
 rbenv rehash
-
-# phpenv PHPビルド依存プログラムのインストール
-sudo apt install -y libxml2-dev
-sudo apt install -y libcurl-dev
-sudo apt install -y libcurl4-gnutls-dev
-sudo apt install -y libjpeg-dev
-sudo apt install -y libtidy-dev
-sudo apt install -y libxslt-dev
-sudo apt install -y autoconf
 
 phpenv install 7.2.6
 phpenv global 7.2.6
