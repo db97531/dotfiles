01715df7462001823e7b0a8b704773dfe2c2b0b9
db97531 <db97531@gmail.com>
2020-09-10 16:16:28 +0900

    Fix win32yank.sh

--- win32yank.sh
+++ win32yank.sh
@@ -3,6 +3,7 @@
 WIN32_YANK=~/.local/bin/win32yank.exe
 
 function get_win32yank {
+    mkdir -p ~/.local/bin/
     curl -L -o /tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
     unzip /tmp/win32yank.zip -d /tmp/win32yank
     mv /tmp/win32yank/win32yank.exe "$WIN32_YANK"
