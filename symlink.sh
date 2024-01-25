6df045967f683ef29974f3411143f591f64cd75e
db97531 <db97531@gmail.com>
2024-01-25 16:36:35 +0900

    starship.tomlを追加

--- symlink.sh
+++ symlink.sh
@@ -16,3 +16,4 @@ ln -sf ~/dotfiles/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/pet/config.toml ~/.config/pet/config.toml
 ln -sf ~/dotfiles/pet/snippet.toml ~/.config/pet/snippet.toml
+ln -sf ~/dotfiles/starship.toml ~/.config/starship.toml
